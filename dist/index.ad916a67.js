6cb365ca6418c83712cb8f27341061ebfb2812ac
Git-Bread <145844979+Git-Bread@users.noreply.github.com>
2024-02-14 19:06:48 +0100

    Delete dist directory
    had some remenant that i removed for clarity

--- dist/index.ad916a67.js
+++
@@ -1,7 +0,0 @@
-//måste ha nån javascript att minfiera
-window.onload = function() {
-    document.getElementById("current").style.color = "red";
-    console.log("hello world");
-};
-
-//# sourceMappingURL=index.ad916a67.js.map
