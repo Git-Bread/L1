304bd725f2d277518d5e3df1d05f67aa88b71d5b
Max <m.gagner@hotmail.se>
2024-02-14 19:00:21 +0100

    Final build of index and images, begining search for webhost.

--- dist/index.ad916a67.js
+++ dist/index.ad916a67.js
@@ -1,2 +1,7 @@
+//måste ha nån javascript att minfiera
+window.onload = function() {
+    document.getElementById("current").style.color = "red";
+    console.log("hello world");
+};
 
 //# sourceMappingURL=index.ad916a67.js.map
---
+++ js/javascript.js
@@ -0,0 +1,5 @@
+//måste ha nån javascript att minfiera
+window.onload = function() {
+    document.getElementById("current").style.color = "red";
+    console.log("hello world");
+};
